8119dcf704402187ca6819992c466e47097465d9
Rafael Tamayo Luisce <rafatmyo@gmail.com>
2019-08-24 23:29:18 -0500

    Heroe Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component'
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 
 
@@ -11,6 +11,7 @@ const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'heroes', component: HeroesComponent},
+    { path: 'heroe/:id', component: HeroeComponent},
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
 ];
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -10,13 +12,19 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor(private _heroesService:HeroesService) {
-    console.log("Contructor")
-  }
+  constructor(private _heroesService:HeroesService,
+              private router:Router
+              ) {
+                console.log("Contructor");
+              }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes)
+    console.log(this.heroes);
+  }
+
+  verHeroe(idx:number){
+    this.router.navigate(['/heroe',idx]);
   }
 
 }
