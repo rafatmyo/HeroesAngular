18d92b64f1ca137356bed163e820909d55f276a0
rafatmyo <rafatmyo@gmail.com>
2019-09-28 22:33:01 -0500

    Added Angular Input OUtput

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -70,9 +70,11 @@ export class HeroesService {
   buscarHeroes(termino:string):Heroe[]{
     let heroesArr:Heroe[] = [];
     termino = termino.toLowerCase();
-    for(let heroe of this.heroes){
+    for(let i = 0; i < this.heroes.length; i++){
+      let heroe = this.heroes[i];
       let nombre = heroe.nombre.toLowerCase();
       if( nombre.indexOf(termino) >= 0){
+        heroe.idx = i;
         heroesArr.push(heroe);
       }
     }
@@ -86,6 +88,7 @@ export interface Heroe {
   bio: string,
   img: string,
   aparicion: string,
-  casa: string
+  casa: string,
+  idx?: number
 
 }
